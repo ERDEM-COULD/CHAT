298b9894400cbdd81ba026ea3c3d323679900e60
ERDEM-COULD <merdem58171.3@gmail.com>
2025-05-03 08:59:41 +0300

    Update themes.js

---
+++ themes.js
@@ -0,0 +1,23 @@
+export const lightTheme = {
+    colors: {
+      primary: '#128C7E',
+      secondary: '#075E54',
+      background: '#f0f2f5',
+      text: '#333',
+      messageSent: '#DCF8C6',
+      messageReceived: '#FFFFFF',
+      inputBackground: '#FFFFFF'
+    }
+  };
+  
+  export const darkTheme = {
+    colors: {
+      primary: '#075E54',
+      secondary: '#128C7E',
+      background: '#121212',
+      text: '#e1e1e1',
+      messageSent: '#005C4B',
+      messageReceived: '#202C33',
+      inputBackground: '#2A3942'
+    }
+  };
